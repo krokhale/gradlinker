062b95a7be0c1efc42efc4958c9a7287cf9138c7
krokhale <krishna.rokhale@gmail.com>
2011-01-21 18:55:52 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gradlinker::Application.config.secret_token = 'c391d7f900bb9a057268cdb8caa86ba10f9e7a9531542af73d19cef3ebed49cae403f15f879ee4062575b4e6887729f8739992831adf337cd21c2954c3d10ff3'
